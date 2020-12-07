7a4a8e08a0bf0e4343e8836326b29bcabb67d7de
asijanah <asafjanah@gmail.com>
2020-12-07 11:09:16 +0100

    download @material-ui/core"

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import{card,cardContent,Typography,Grid} from '@material-ui/core';
+
+import{getCovid} from '../../api/index'
 
 const Cards=()=>{
+const[covidData,setCovidData]=useState([]);
+
+useEffect(async ()=>{
+    const response= await getCovid();
+    setCovidData(response)
+},[])
+console.log(covidData.data.deaths.value);
+
+
     return (
         <div className="Cards"> CARDS</div>
     )
