271eaeb0d1e70618899de6c46be58e0f4696ceb5
asijanah <asafjanah@gmail.com>
2020-12-07 12:46:38 +0100

    Add cardes

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,20 +1,57 @@
-import React,{useState,useEffect} from 'react';
-import{card,cardContent,Typography,Grid} from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
 
-import{getCovid} from '../../api/index'
+import styles from './Cards.module.css'
+import { getCovid } from '../../api/index'
 
-const Cards=()=>{
-const[covidData,setCovidData]=useState([]);
+const Cards = () => {
+    const [covidData, setCovidData] = useState([]);
 
-useEffect(async ()=>{
-    const response= await getCovid();
-    setCovidData(response)
-},[])
-console.log(covidData.data.deaths.value);
+    useEffect(async () => {
+        const response = await getCovid();
+        setCovidData(response)
+    }, [])
+    // console.log(covidData.data.deaths.value);
 
 
     return (
-        <div className="Cards"> CARDS</div>
+
+        <div className={styles.container}>
+             {covidData.data &&
+            <Grid container spacing={3} justify='center'>
+               
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>{covidData.data.confirmed.value}</Typography>
+                        <Typography variant='h5'>new da</Typography>
+                        <Typography color='textSecondary'>date</Typography>
+                        <Typography variant='body2'>Number of Active Cases</Typography>
+                    </CardContent>
+                </Grid>
+    
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>{covidData.data.recovered.value}</Typography>
+                        <Typography variant='h5'>Data</Typography>
+                        <Typography color='textSecondary'>date</Typography>
+                        <Typography variant='body2'>Number of Recoverd Cases</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>{covidData.data.deaths.value}</Typography>
+                        <Typography variant='h5'>Data</Typography>
+                        <Typography color='textSecondary'>date</Typography>
+                        <Typography variant='body2'>Number of Deaths Cases</Typography>
+                    </CardContent>
+                </Grid>
+                
+            </Grid>
+                }
+        </div>
     )
 }
 export default Cards
