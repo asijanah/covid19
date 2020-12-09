fe7c9df61a8c25bea40f356a65040be51aeda929
asijanah <asafjanah@gmail.com>
2020-12-09 11:04:25 +0100

    add line chart

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <Cards/>
       <Chart/>
       <CountryPicker/>
-
     </div>
   );
 }
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,25 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import { Line, Bar } from 'react-chartjs-2';
 
-import{getDaily} from '../../api/index';
+import { getDaily } from '../../api/index';
+import styles from './Chart.module.css';
+
 
 const Chart = () => {
 
-    const[dailyData,setDailyData]=useState([])
+    const [dailyData, setDailyData] = useState([])
 
-    useEffect(async() => {
+    useEffect(async () => {
         const response = await getDaily();
         setDailyData(response.data)
-    },[])
+    }, [])
+
     console.log('dailyData: ');
     console.log(dailyData);
 
+
+    const dates= dailyData.map(item =>item.reportDate)
+    const confirmed=dailyData.map(item =>item.totalConfirmed)
+    const deaths= dailyData.map(item =>item.deaths.total)
+
+    const lineChart = (
+        <Line
+            data={{
+                labels: dates,
+                datasets: [{label:'confirmed',data:confirmed,fill:true}, 
+                {label:'deaths',data:deaths,fill:true}]
+            }}
+        />
+    )
+
+
+
     return (
-        <div>
-        {
-            dailyData.map(day=><ul><li>{day.totalConfirmed}</li></ul>)
-        }
+        <div className={styles.container}>
+            {lineChart}
         </div>
-        // <div className="chart"> CHART </div>
     )
 }
 export default Chart
